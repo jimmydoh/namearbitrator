0ac45e7c761db0056fdffea3d2e58242a52a245f
James Douglas <jimmydoh@gmail.com>
2017-04-13 21:58:41 +1000

    Corrected error in settings.py

--- namearbitrator/settings.py
+++ namearbitrator/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'jadauth.apps.NamepollConfig',
+	'jadauth.apps.JadauthConfig',
 	'namepoll.apps.NamepollConfig',
 ]
 
